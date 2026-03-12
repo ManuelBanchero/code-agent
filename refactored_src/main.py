1ea0eb20533dbcfea5aacecb3eead6de73eee034
ManuelBanchero <manuelbanchero04@gmail.com>
2026-03-12 09:45:15 -0300

    WriteFile tool added

--- refactored_src/main.py
+++ refactored_src/main.py
@@ -2,6 +2,7 @@ from config.config import WORKING_DIRECTORY, MAX_MODEL_CALLS, AI_MODEL, MAX_CHAR
 from utils.prompts import system_prompt
 from model.Tool.GetFilesInfo import GetFilesInfo
 from model.Tool.GetFileContent import GetFileContent
+from model.Tool.WriteFile import WriteFile
 from model.Agent import Agent
 from dotenv import load_dotenv
 import os
@@ -14,10 +15,13 @@ if api_key == None:
 get_files_info = GetFilesInfo(working_directory=WORKING_DIRECTORY)
 get_file_content = GetFileContent(
     working_directory=WORKING_DIRECTORY, max_chars=MAX_CHARS)
+write_file = WriteFile(working_directory=WORKING_DIRECTORY)
+
 
 tools = {
     'get_files_info': get_files_info,
-    'get_file_content': get_file_content
+    'get_file_content': get_file_content,
+    'write_file': write_file
 }
 
 agent = Agent(
@@ -28,7 +32,8 @@ agent = Agent(
     API_KEY=api_key
 )
 
-response, success = agent.query('get main.py content')
+response, success = agent.query(
+    'modify lorem.txt and write: "This is a new test."')
 if success:
     print(response)
 else:
---
+++ refactored_src/model/Tool/WriteFile.py
@@ -0,0 +1,48 @@
+from model.Tool.Tool import Tool
+from model.Tool.Schema import Schema
+import os
+
+
+class WriteFile(Tool):
+    def __init__(self, working_directory):
+        super().__init__(working_directory)
+        self.__schema = Schema(
+            name='write_file',
+            descripcion='Writes text content to a specified file path, automatically creating any missing parent directories and ensuring the operation stays within the permitted working directory',
+            file_path={
+                'type': 'string',
+                'description': 'he relative path to the target file, starting from the current working directory. It must stay within the permitted directory structure and cannot be an absolute path or use ".." to escape the root.',
+                'required': True
+            },
+            content={
+                'type': 'string',
+                'description': 'The text data to be written into the file. The function will overwrite any existing content in the file with this new string.',
+                'required': True
+            }
+        )
+
+    def get_schema(self):
+        return self.__schema.get_schema()
+
+    def execute(self, file_path, content):
+        try:
+            working_dir_abs = os.path.abspath(self._working_directory)
+            target_file = os.path.normpath(
+                os.path.join(self._working_directory, file_path))
+
+            valid_target_dir = os.path.commonpath(
+                [working_dir_abs, target_file]) == working_dir_abs
+
+            if not valid_target_dir:
+                return f'  Error: Cannot write to "{file_path}" as it is outside the permitted working directory'
+            if os.path.isdir(target_file):
+                return f'  Error: Cannot write to "{file_path}" as it is a directory'
+
+            os.makedirs(os.path.dirname(target_file), exist_ok=True)
+
+            with open(target_file, 'w') as f:
+                f.write(content)
+
+            return f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
+        except Exception as e:
+            return f'Error: {e}'
